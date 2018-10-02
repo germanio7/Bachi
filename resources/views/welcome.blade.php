4eb93791cd1d0d77e7ddac7b9409c48ba0216954
Carlos <zalazaarcarlos@gmail.com>
2018-10-02 18:34:56 -0300

    primercommit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Soy el Guason
                 </div>
 
                 <div class="links">
