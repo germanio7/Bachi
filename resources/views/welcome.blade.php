0e738aea7de4beefff13bd9d602ecf42d2fc3bb9
Maxi <kazmermaximiliano@gmail.com>
2018-10-22 07:36:27 -0300

    welcome con datos de cursos y alumnos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,6 @@
 
 @section('contenido')
 
-
 <nav class="hide-on-med-and-up light-green">
   <div class="nav-wrapper">
     <a href="#!" class="brand-logo">Bachi</a>
@@ -51,32 +50,101 @@
   </div>
 
   <div class="col s12 m8 l9">
+
+    <div class="parallax-container">
+        <div class="parallax"><img src="{{'/img/BachiFondo2.png'}}" width="auto" height="1080"></div>
+      </div>
+
     <div class="container">
 
-      <h1 class="center-align green-text text-light">Bachi</h1>
-      <br>
-      <h4 class="center-align">E.E.S Nº. 9 "Maestro Sarmiento"</h4>
       <br><br>
 
-      <div class="col s12">
-        <div class="card horizontal">
-          <div class="card-image">
-            <img src="https://lorempixel.com/200/200/nature/6">
-          </div>
-          <div class="card-stacked">
-            <div class="card-content">
-              <p><h3>1º Ciclo Basico</h3></p>
+      @foreach($cursos as $curso)
+
+          <div class="col s12">
+            <div class="card horizontal">
+              <div class="card-stacked">
+
+                <div class="card-content">
+                  <span id="bars<?php echo $curso->id ?>" data-id="{{$curso->id}}" style="display: inline;" class="card-title activator grey-text text-darken-4"><b>{{$curso->curso}} {{$curso->orientacion}}</b><i class="fas fa-bars fa-lg right"></i></span>
+                  <span id="times<?php echo $curso->id ?>" data-id="{{$curso->id}}" style="display: none;" class="card-title activator grey-text text-darken-4"><b>{{$curso->curso}} {{$curso->orientacion}}</b><i class="fas fa-times fa-lg right"></i></span>
+                </div>
+                <div id="tabss<?php echo $curso->id ?>" data-id="{{$curso->id}}" style="display: none;">
+                  <div class="card-action">
+                    <div class="card-tabs">
+                      <ul class="tabs tabs-fixed-width">
+                        <li class="tab"><a href="#materias<?php echo $curso->id ?>" class="green-text text-light-darken-2">Materias</a></li>
+                        <li class="tab"><a href="#alumnos<?php echo $curso->id ?>" class="green-text text-light-darken-2 active">Alumnos</a></li>
+                        <li class="tab"><a href="#docentes<?php echo $curso->id ?>" class="green-text text-light-darken-2">Docentes</a></li>
+                      </ul>
+                    </div>
+                    <div class="card-content">
+                      <div id="materias<?php echo $curso->id ?>">Materias</div>
+                      <div id="alumnos<?php echo $curso->id ?>">
+                        <table class="highlight">
+                          <thead>
+                            <tr>
+                              <th>Cuil</th>
+                              <th>Apellido</th>
+                              <th>Nombre</th>
+                              <th></th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            @foreach($alumnocurso as $ac)
+                              @foreach($alumnos as $alumno)
+                                @if($alumno->id == $ac->alumno_id && $curso->id == $ac->curso_id)
+                                  <tr>
+                                    <td>{{$alumno->cuil}}</td>
+                                    <td>{{$alumno->apellido}}</td>
+                                    <td>{{$alumno->nombre}}</td>
+                                    <td>
+                                      <div class="btn-group" role="group">
+                                        <a class="btn blue" href="#"><i class="fas fa-print"></i></a>
+                                        <a class="btn green" href="#"><i class="fas fa-pen"></i></a>
+                                        <a class="btn red" href="#"><i class="fas fa-trash"></i></a>
+                                      </div>
+                                    </td>
+                                  </tr>
+                                @endif
+                              @endforeach
+                            @endforeach      
+                          </tbody>
+                        </table>
+                      </div>
+                      <div id="docentes<?php echo $curso->id ?>">Docentes</div>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
             </div>
-            <div class="card-action">
-              <a>3 Alumnos</a>
-            </div>
           </div>
-        </div>
-      </div>
+
+      @endforeach
           
     </div>
   </div>
 
 </div>
+
+<script src="{{'/js/jquery.js'}}"></script>
+@foreach($cursos as $curso)
+<script>
+  $(document).on("click", "#bars"+{{$curso->id}}, function(){
+
+      var id = $(this).data("id");
+      hola(id);
+
+  })
+  $(document).on("click", "#times"+{{$curso->id}}, function(){
+
+      var id = $(this).data("id");
+      hola(id);
+
+  })
+</script>
+@endforeach()
+<script src="{{'/js/funciones.js'}}"></script>
 
 @endsection
