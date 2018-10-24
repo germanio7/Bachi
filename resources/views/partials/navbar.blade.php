e23abd4697160e1b05509bb1bb023fb0402c007f
Maxi <Maxi@gmail>
2018-10-24 09:40:57 -0300

    vistas arregladasçç

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,8 +2,11 @@
 	<nav>
 		<div class="nav-wrapper light-green">
 			<a href="#" class="brand-logo">Logo</a>
-			<ul id="nav-mobile" class="right hide-on-med-and-down">				
-			</ul>
+      <div class="container">
+  			<ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="{{'/'}}" class="white-text"><i class="fas fa-home fa-2x"></i></a></li>				
+  			</ul>
+      </div>
 		</div>
 	</nav>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
   <ul class="collapsible popout">
     @foreach($cursos as $curso)
       <li>
-        <div class="collapsible-header"><h6><b>{{$curso->curso}} {{$curso->orientacion}}</b></h6></div>
+        <div class="collapsible-header hoverable"><h6><b>{{$curso->curso}} {{$curso->orientacion}}</b></h6></div>
         <div class="collapsible-body">
           <div class="row">
             <div class="col s12">
@@ -31,16 +31,18 @@
                   @foreach($alumnocurso as $ac)
                     @foreach($alumnos as $alumno)
                       @if($alumno->id == $ac->alumno_id && $curso->id == $ac->curso_id)
-                        <td>{{$alumno->cuil}}</td>
-                        <td>{{$alumno->apellido}}</td>
-                        <td>{{$alumno->nombre}}</td>
-                        <td>
-                          <div class="btn-group" role="group">
-                            <a class="btn blue" href="#"><i class="fas fa-print fa-lg"></i></a>
-                            <a class="btn green" href="#"><i class="fas fa-pen fa-lg"></i></a>
-                            <a class="btn red" href="#"><i class="fas fa-trash fa-lg"></i></a>
-                          </div>
-                        </td>
+                        <tr>
+                          <td>{{$alumno->cuil}}</td>
+                          <td>{{$alumno->apellido}}</td>
+                          <td>{{$alumno->nombre}}</td>
+                          <td>
+                            <div class="btn-group" role="group">
+                              <a class="btn blue" href="#"><i class="fas fa-print fa-lg"></i></a>
+                              <a class="btn green" href="#"><i class="fas fa-pen fa-lg"></i></a>
+                              <a class="btn red" href="#"><i class="fas fa-trash fa-lg"></i></a>
+                            </div>
+                          </td>
+                        </tr>
                       @endif
                     @endforeach
                   @endforeach  
@@ -57,27 +59,30 @@
                   </tr>
                 </thead>
                 <tbody>
-                  @foreach($cursomateria as $cm)
-                    @foreach($materias as $materia)
-                      @if($materia->id == $cm->materia_id && $curso->id == $cm->curso_id)
-                        <td>{{$materia->nombre}}</td>
+                    @foreach($cursomateria as $cm)
+                      @foreach($materias as $materia)
                         @foreach($docentemateria as $dm)
                           @foreach($docentes as $docente)
-                            @if($materia->id == $dm->materia_id && $docente->id == $dm->docente_id)
-                              <td>{{$docente->apellido}} {{$docente->nombre}}</td>
+                            @if($materia->id == $cm->materia_id && $curso->id == $cm->curso_id)
+                              @if($materia->id == $dm->materia_id && $docente->id == $dm->docente_id)
+                                <tr>
+                                  <td>{{$materia->nombre}}</td>
+                                      
+                                        <td>{{$docente->apellido}} {{$docente->nombre}}</td>
+                                  <td>
+                                    <div class="btn-group" role="group">
+                                      <a class="btn blue" href="#"><i class="fas fa-print fa-lg"></i></a>
+                                      <a class="btn green" href="#"><i class="fas fa-pen fa-lg"></i></a>
+                                      <a class="btn red" href="#"><i class="fas fa-trash fa-lg"></i></a>
+                                    </div>
+                                  </td>
+                                </tr>
+                              @endif
                             @endif
                           @endforeach
                         @endforeach
-                        <td>
-                          <div class="btn-group" role="group">
-                            <a class="btn blue" href="#"><i class="fas fa-print fa-lg"></i></a>
-                            <a class="btn green" href="#"><i class="fas fa-pen fa-lg"></i></a>
-                            <a class="btn red" href="#"><i class="fas fa-trash fa-lg"></i></a>
-                          </div>
-                        </td>
-                      @endif
-                    @endforeach
-                  @endforeach  
+                      @endforeach
+                    @endforeach  
                 </tbody>
               </table>
             </div>
